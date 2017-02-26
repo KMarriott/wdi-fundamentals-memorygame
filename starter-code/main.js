8d7936f7b0e520493b192c07893f199cde780bbb
Khem M <Khem M>
2017-02-26 05:11:41 -0500

    create game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,11 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOme = "queen"
+var cardOne = "queen"
 var cardTwo = "queen"
 var cardThree = "king"
 var cardFour = "king"
+
+if(cardOne === cardTwo)
+{alert("You found a match!")}
+else
+{alert("Sorry, try again.")}	
