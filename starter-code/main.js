02748475cd2935ea739d589d679eb5f94d245614
Khem M <Khem M>
2017-02-27 01:13:10 -0500

    create game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,21 +1,46 @@
-console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen"
-var cardTwo = "queen"
-var cardThree = "king"
-var cardFour = "king"
+var cards = ['Queen', 'Queen', 'King', 'King'];
+var cardsInPlay = [];
 
-if(cardOne === cardTwo)
-{alert("You found a match!")}
-else
-{alert("Sorry, try again.")}	
+var board = document.getElementById('game-board');
 
-var createCards = function(i){
-for (var i=0; i < 5; i++) {
-	var board = document.getElementById('game-board');
-	var newCardElement = document.createElement('div');
-	newCardElement.className = ('card');
-	board.appendChild(newCardElement);
-}
-}
-createCards();
+function createBoard() {
+	for (var i=0; i<cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+
+		board.appendChild(cardElement);
+	}
+};
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='king.jpg' alt='King of Spades'/>"; // King image
+	} else {
+		this.innerHTML = "<img src='queen.jpg' alt='Queen of Hearts'/>"; //Queen image
+	}
+
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+};
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Please try again!");
+	}
+			for (var i=0; i<cards.length; i++) {
+			if (document.getElementsByClassName('card')[i]) {
+				document.getElementsByClassName('card')[i].innerHTML = "";
+			}
+		}
+};
+
+
+createBoard();
